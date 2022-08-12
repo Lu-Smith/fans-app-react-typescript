e7dc8fdbe83c39b46fbca2151f5de504590c7aac
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-12 18:24:35 +0100

    Added links.json
    Updated NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import "../styles/NavBar.css";
 import { IoPawSharp } from 'react-icons/io5';
+import * as data from './links.json';
+const linksString = JSON.stringify(data);
+const links = JSON.parse(linksString).links;
+
+
+type Link ={
+  label: string;
+  href: string;
+}
 
 const NavBar: React.FC<{}> = () => {
   return (
     <nav className='NavBar'>
       <div className='logo-container'>
         <span><IoPawSharp className='logo-img'/>PawHub</span>
+      </div>
+      <div className='menu-container'>
+        {links.map((link: Link) => {
+          return (
+            <div className='menu-link' key={link.href}>
+              <a href={link.href}>
+                {link.label}
+              </a>
+            </div>
+          )
+        })}
       </div>
     </nav>
   )
