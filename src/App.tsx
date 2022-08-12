d64484d2951e2561a12bb814100e4cffeb7a220e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-12 09:25:48 +0100

    Added main folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello World</h1>
+      <NavBar />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+         <h1> PawHub is the place that rewards real Pets lovers</h1>
+    </div>
+  )
+}
+
+export default Main;
