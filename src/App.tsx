f04d0a715aa645a06655a91d192f770156d31728
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-12 09:47:42 +0100

    Updated App.tsx and Main.tsx
    Added AddMeber.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import NavBar from './components/NavBar';
 
+export interface IState {
+  members: {
+    name: string
+    age: number
+    img: string
+    note: string
+    pet: string
+    petAge: number
+  }[]
+}
+
 function App() {
+const [members, setMembers] = useState<IState["members"]>([
+  {
+    name: "string",
+    age: 54,
+    img: "string",
+    note: "string",
+    pet: "string",
+    petAge: 4,
+  },
+  {
+    name: "string",
+    age: 54,
+    img: "string",
+    note: "string",
+    pet: "string",
+    petAge: 4,
+  },
+])
+
   return (
     <div className="App">
       <NavBar />
-      <Main />
+      <Main members={members} />
       <Footer />
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { IState as Props } from '../App';
+import AddMember from './AddMember';
 
-const Main = () => {
+interface IProps {
+  members: Props["members"]
+}
+
+const Main: React.FC<IProps> = ( {members}) => {
+
+const renderMembers = (): JSX.Element[] => {
+  return members.map(member => {
+    return (
+      <li>{member.name}</li>
+    )
+  })
+}
+
   return (
     <div>
          <h1> PawHub is the place that rewards real Pets lovers</h1>
+         <ul>
+           {renderMembers()}
+         </ul>
+         <AddMember />
     </div>
   )
 }
