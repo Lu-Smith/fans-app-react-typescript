6d42c1e2771be0d520e549859b60bf2e0e937cda
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-12 11:04:34 +0100

    Added Main.css
    Mainor changes to Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,32 @@ import NavBar from './components/NavBar';
 
 export interface IState {
   members: {
-    name: string
-    age: number
+    ownerName: string
     img: string
     note: string
     pet: string
     petAge: number
+    petName: string
   }[]
 }
 
 function App() {
 const [members, setMembers] = useState<IState["members"]>([
   {
-    name: "Alice Chow",
-    age: 54,
+    ownerName: "Alice Chow",
     img: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     note: "“No matter how you're feeling, a little dog gonna love you.”",
     pet: "Cat",
     petAge: 4,
+    petName: "Donald"
   },
   {
-    name: "Harry S Truman",
-    age: 54,
+    ownerName: "Harry S Truman",
     img: "https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     note: "“You want a friend in Washington? Get a dog.”",
     pet: "Dog",
     petAge: 6,
+    petName: "Spark",
   },
 ])
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IState as Props } from '../App';
 import AddMember from './AddMember';
+import "../styles/Main.css";
 
 interface IProps {
   members: Props["members"]
@@ -14,19 +15,32 @@ const renderMembers = (): JSX.Element[] => {
       <li>
         <div>
           <img src={member.img} alt="${member.name} pet" />
-          <h2>{member.name}</h2>
+          <h2>{member.petName}</h2>
         </div>
-        <p>{member.age}</p>
-        <p>{member.note}</p>
-        <p>{member.petAge}</p>
-        <p>{member.pet}</p>
+        <div>
+        <div >
+          <h3>PET</h3>
+          <div className='petInfo'>
+          <p>age: {member.petAge}</p>
+          <p>species: {member.pet}</p>
+          </div>
+        </div>
+        <div className='ownerInfo'>
+          <h3>OWNER</h3>
+          <p>{member.ownerName}</p>
+          <p>{member.note}</p>
+        </div>
+        </div>
+     
+       
+        
         </li>
     )
   })
 }
 
   return (
-    <div>
+    <div className='Main'>
          <h1> PawHub is the place that rewards real Pets lovers</h1>
          <ul>
            {renderMembers()}
