9d1258b41f8a97ca34ee8f48ceee3ca716efa427
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-12 09:56:29 +0100

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith</a>, open source on <a href="https://github.com/Lu-Smith/fans-app-react-typescript" target="_blank" rel="noreferrer">GitHub</a></div>
   )
 }
 
